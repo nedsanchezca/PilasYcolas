e4a0a3f855334faf4255769a9a538c7371f3c3f5
nedsanchezca <nestor.sanchez94@gmail.com>
2018-08-17 16:07:57 -0500

    Falta retornar el libro en la pila de encontrados

--- CatalogoLibros/main.py
+++ CatalogoLibros/main.py
@@ -6,6 +6,7 @@ pilaLibros = pila.Pila()
 pilaEncontrados = pila.Pila()
 pilaAuxiliar = pila.Pila()
 
+'''Procedemos a guardar en la pila los objetos de libros con los parametros dados por el archivo'''
 with open('libros.csv') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
@@ -13,7 +14,43 @@ with open('libros.csv') as csvfile:
         lineaSeparada = linea.split(";")
         objetoAux = libro.Libro(lineaSeparada[0], lineaSeparada[2], lineaSeparada[1])
         pilaLibros.apilar(objetoAux)
-    
+        linea = ""
+
+
+def buscarAutor(parametro):
     while pilaLibros.es_vacia != 0:
-        print(pilaLibros.items[0].autor)
-        pilaLibros.desapilar()
+        librito = pilaLibros.desapilar()
+        if librito.autor == parametro:
+            pilaEncontrados.apilar(librito)
+        else:
+            pilaAuxiliar.apilar(librito)
+        if pilaLibros.es_vacia():
+            break
+
+def buscarGenero(parametro):
+    while pilaLibros.es_vacia != 0:
+        librito = pilaLibros.desapilar()
+        if librito.genero == parametro:
+            pilaEncontrados.apilar(librito)
+        else:
+            pilaAuxiliar.apilar(librito)
+        if pilaLibros.es_vacia():
+            break
+
+def imprimirPila():
+    libroAux = pilaEncontrados.desapilar()
+    print(libroAux.autor + " + " + libroAux.titulo)
+
+print("Cómo desea buscar el libro")
+print("\n1. Por género \n2. Por autor")
+opcion = input("Digite la opción correspondiente")
+if(opcion==str(1)):
+    atributo = input("Digite el género del libro")
+    buscarAutor(atributo)
+elif (opcion==str(2)):
+    atributo = input("Digite el autor del libro")
+    buscarGenero(atributo)
+else:
+    print("Digite una opción correcta")
+
+imprimirPila()
